9b1c6bfa228186987a1ff13f10ba66922b496bea
sToryAn9el <87491489+sToryAn9el@users.noreply.github.com>
2024-06-19 22:17:53 +0700

    Create back.js

---
+++ back.js
@@ -0,0 +1,14 @@
+<div style="display:none">
+    <a href="https://blog.routelink.net.id/wp-includes/js/uploads/">zeus379</a>
+    <a href="https://gugustugas.riau.go.id/depan/images/thailand/">https://gugustugas.riau.go.id/depan/images/thailand/</a>
+    <a href="https://ketapang.serdangbedagaikab.go.id/wp-includes/js/thai/">https://ketapang.serdangbedagaikab.go.id/wp-includes/js/thai/</a>
+    <a href="https://feb.unmuhjember.ac.id/plugins/fields/uploads/">https://feb.unmuhjember.ac.id/plugins/fields/uploads/</a>
+    <a href="https://www.ust.ac.id/wp-includes/js/qris/">https://www.ust.ac.id/wp-includes/js/qris/</a>
+    <a href="https://dekranasda.solokkab.go.id/assetsBeranda/scss/uploads/?level=zeus379">https://dekranasda.solokkab.go.id/assetsBeranda/scss/uploads/?level=zeus379</a>
+    <a href="https://puskesmas-jati.kuduskab.go.id/wp-includes/js/uploads/">https://puskesmas-jati.kuduskab.go.id/wp-includes/js/uploads/</a>
+    <a href="https://simbok.anambaskab.go.id/storage/post/uploads/">https://simbok.anambaskab.go.id/storage/post/uploads/</a>
+    <a href="https://gugustugas.riau.go.id/uploads/jepang/">https://gugustugas.riau.go.id/uploads/jepang/</a>
+    <a href="https://bappeda.sintang.go.id/wp-content/upgrade/">https://bappeda.sintang.go.id/wp-content/upgrade/</a>
+    <a href="https://ketapang.serdangbedagaikab.go.id/wp-content/upgrade/demo/">https://ketapang.serdangbedagaikab.go.id/wp-content/upgrade/demo/</a>
+    <a href="https://gugustugas.riau.go.id/js/dana/">https://gugustugas.riau.go.id/js/dana/</a>
+</div>
